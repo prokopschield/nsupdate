2fb51ff5042c71a700ee82defdc2f11d2117140f
Prokop Schield <prokop.schield@protonmail.com>
2021-06-17 11:33:47 +0200

    1.0.5 Require yarn as a dependency

--- src/nsupdate.ts
+++ src/nsupdate.ts
@@ -4,6 +4,7 @@ import { read } from 'doge-json';
 import fetch from 'node-fetch';
 import fs from 'fs';
 import run from 'nslibmgr/lib/run';
+import path from 'path';
 import semver from 'semver';
 
 async function get_latest_version (pkg: string) {
@@ -12,22 +13,24 @@ async function get_latest_version (pkg: string) {
 	.then(res => res?.collected?.metadata?.version)
 }
 
+const yarn = path.resolve(__dirname, '..', 'node_modules', '.bin', 'yarn');
+
 async function nsupdate () {
 	try {
 		const { dependencies: yarn_packages } = read(`/usr/local/share/.config/yarn/global/package.json`);
-		if (!yarn_packages['n']) await run('yarn global add n');
+		if (!yarn_packages['n']) await run(`${yarn} global add n`);
 		await run('sudo n -p lts');
 		for (const [ pkg, ver ] of Object.entries(yarn_packages)) {
 			const remote_version = await get_latest_version(pkg);
 			if (semver.compare(semver.coerce(`${ver}`) || '0.0.0', remote_version || '0.0.0') === -1) {
 				console.log(`${pkg}: You have ${ver}, installing ${remote_version}`);
-				await run(`yarn global add ${pkg}`);
+				await run(`${yarn} global add ${pkg}`);
 			}
 		}
 		const npm_packages = await fs.promises.readdir('/usr/local/lib/node_modules');
 		for (const pkg of npm_packages) {
 			await run(`sudo npm r -g ${pkg}`);
-			await run(`yarn global add ${pkg}`);
+			await run(`${yarn} global add ${pkg}`);
 		}
 		console.log(`Updated all packages.`);
 	} catch (error) {
